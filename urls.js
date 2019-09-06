2ff91ad0515a8802d25e88f733e5d7e22d8fb203
takuto <takuto.yoshikai@gmail.com>
2019-09-06 23:24:12 +0900

    change url

--- urls.js
+++ urls.js
@@ -4,6 +4,6 @@ var json = JSON.parse(fs.readFileSync("repos", "utf-8"));
 console.log("#!/bin/bash");
 console.log("cd " + process.argv[3]);
 json.forEach(function(name) {
-	const cmd = "git clone https://github.com/" + process.argv[2] + "/" + name;
+	const cmd = "git clone git@github.com:" + process.argv[2] + "/" + name;
 	console.log(cmd);
 });
